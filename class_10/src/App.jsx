c2e98a90ca0cb406ccc464ae21555bf1ff2a3c52
mayank <mayanksharma231020@gmail.com>
2026-06-10 16:23:09 +0530

    feat: integrate React Router for navigation and add Home, About, and Products pages

--- class_10/src/App.jsx
+++ class_10/src/App.jsx
@@ -1,43 +1,22 @@
-import { useState}from 'react'
+import { useState , useEffect}from 'react'
 import './App.css'
 import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Products from './pages/products'
 
 
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [response, setResponse] = useState({ data: [] });
-
-  useEffect( () => {
-    const fetchdata =async () => {
-      const responses = await axios.get('https://jsonplaceholder.typicode.com/users')
-      setResponse(responses);
-    };
-    fetchdata();
-  },[])
-
-
   return (
     <div>
-      <h1 >Counter: {counter}</h1>
-      <div id='container'>
-        {response.data.map(user => {
-          let c1=Math.floor(Math.random() * 256);
-          let c2=Math.floor(Math.random() * 256);
-          let c3=Math.floor(Math.random() * 256);
-
-          return(
-          <div className="box" style={{ backgroundColor: `rgb(${c1}, ${c2}, ${c3})` }} key={user.id}>
-            <h2>{user.name}</h2>
-            <p>{user.website}</p>
-          </div>)
-        }
-        )}
-      </div>
-      <button onClick={() => setCounter(counter + 1)}>
-        Increment
-      </button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </div>
   )
 }
